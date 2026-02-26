3a6bc5e9db7f359b3c334e6d7d213432918001ed
Steven Widjaja <widjajs@purdue.edu>
2026-02-25 23:05:45 -0500

    surround fixes

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,19 +1,27 @@
--- ~/.config/nvim/lua/custom/plugins.lua
 return {
     {
         "kylechui/nvim-surround",
         version = "*",
+        event = "VeryLazy",
+        lazy = false,
+        init = function()
+            vim.g.nvim_surround_no_mappings = true
+            vim.g.nvim_surround_no_insert_mappings = true
+        end,
         config = function()
-            require("nvim-surround").setup({
-                keymaps = {
-                    insert = "<C-g>s",
-                    visual = "gs",
-                    normal = "ys",
-                    normal_cur = "yss", -- current line
-                    delete = "ds", -- delete surrounding
-                    change = "cs", -- change surrounding
-                },
-            })
+            require("nvim-surround").setup({ highlight = { duration = 500 } })
+            vim.keymap.set("n", "ys", "<Plug>(nvim-surround-normal)", { remap = true, desc = "Add surround" })
+            vim.keymap.set("n", "yss", "<Plug>(nvim-surround-normal-cur)", { remap = true, desc = "Add surround line" })
+            vim.keymap.set("v", "S", "<Plug>(nvim-surround-visual)", { remap = true, desc = "Add surround visual" })
+            vim.keymap.set("v", "gs", "<Plug>(nvim-surround-visual)", { remap = true, desc = "Add surround gs" })
+            vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)", { remap = true, desc = "Delete surround" })
+            vim.keymap.set("n", "cs", "<Plug>(nvim-surround-change)", { remap = true, desc = "Change surround" })
+            vim.keymap.set(
+                "i",
+                "<C-g>s",
+                "<Plug>(nvim-surround-insert)",
+                { expr = true, remap = true, desc = "Add surround insert" }
+            )
         end,
     },
 }
